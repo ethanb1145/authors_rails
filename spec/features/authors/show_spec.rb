23eeedddd33aeef1c96b8e0e06c02799f4b5155b
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-24 18:34:57 -0600

    Fix: correct the spec test

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'the authors show page' do
+RSpec.describe Author do
   describe 'validations' do
     it {should validate_presence_of :name}
   end
 
-  describe 'relationships' do
-    it {should have_many :songs}
+  xdescribe 'relationships' do
+    it {should have_many :books}
   end
 end
