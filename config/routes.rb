37936c0cffb9427b967a812c5f1046c5b5c6ce91
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-25 16:03:35 -0600

    Setup routes for Books

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
   get "/authors/:id/edit", to: "authors#edit"
   patch "/authors/:id", to: "authors#update"
   delete "/authors/:id", to: "authors#destroy"
+
+  get "/", to: "welcome#index"
+  get "/books", to: "books#index"
+  get "/books/new", to: "books#new"
+  post "/books", to: "books#create"
+  get "/books/:id", to: "books#show"
+  get "/books/:id/edit", to: "books#edit"
+  patch "/books/:id", to: "books#update"
+  delete "/books/:id", to: "books#destroy"
 end
