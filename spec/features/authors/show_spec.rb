349f328cbafbd9068946efd1ccfef12e09028631
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-26 13:30:36 -0600

    Test for User Story 2

--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -1,21 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "authors show page", type: :feature do
-  before :each do 
-    @author1 = Author.create(name: "J.K. Rowling", hometown: "Yate, England", currently_writing: true, total_awards: 20)
-    @author2 = Author.create(name: "George R.R. Martin", hometown: "Bayonne, New Jersey", currently_writing: false, total_awards: 5)
-    @author3 = Author.create(name: "Stephen King", hometown: "Portland, Maine", currently_writing: true, total_awards: 15)
-  end
-
   it "As a visitor
   When I visit '/parents/:id'
   Then I see the parent with that id including the parent's attributes
   (data from each column that is on the parent table)" do
-    visit "/authors/1"
+    author1 = Author.create(name: "J.K. Rowling", hometown: "Yate, England", currently_writing: true, total_awards: 20)
 
-    expect(page).to have_content(@author1.name)
-    expect(page).to have_content(@author1.hometown)
-    expect(page).to have_content(@author1.currently_writing)
-    expect(page).to have_content(@author1.total_awards)
+    visit "/authors/#{author1.id}"
+
+    expect(page).to have_content(author1.name)
+    expect(page).to have_content(author1.hometown)
+    expect(page).to have_content(author1.currently_writing)
+    expect(page).to have_content(author1.total_awards)
   end
 end
