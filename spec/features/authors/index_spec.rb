6d4fa13f79a9e727722882788e04b03725c1d23d
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-26 13:32:41 -0600

    Replace with correct words

--- spec/features/authors/index_spec.rb
+++ spec/features/authors/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "authors index page", type: :feature do
   it "For each parent table
   As a visitor
-  When I visit '/parents'
-  Then I see the name of each parent record in the system" do
+  When I visit '/authors'
+  Then I see the name of each author record in the system" do
     author1 = Author.create(name: "J.K. Rowling", hometown: "Yate, England", currently_writing: true, total_awards: 20)
     author2 = Author.create(name: "George R.R. Martin", hometown: "Bayonne, New Jersey", currently_writing: false, total_awards: 5)
     author3 = Author.create(name: "Stephen King", hometown: "Portland, Maine", currently_writing: true, total_awards: 15)
--- spec/features/authors/show_spec.rb
+++ spec/features/authors/show_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "authors show page", type: :feature do
   it "As a visitor
-  When I visit '/parents/:id'
-  Then I see the parent with that id including the parent's attributes
-  (data from each column that is on the parent table)" do
+  When I visit '/authors/:id'
+  Then I see the author with that id including the authors's attributes
+  (data from each column that is on the author table)" do
     author1 = Author.create(name: "J.K. Rowling", hometown: "Yate, England", currently_writing: true, total_awards: 20)
 
     visit "/authors/#{author1.id}"
