a368783e7c34b22c0f4d5afc908078a20ba45748
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-29 14:29:51 -0600

    Create new author test

---
+++ spec/features/authors/new_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "author creation", type: :feature do 
+  it "As a visitor
+  When I visit the Parent Index page
+  Then I see a link to create a new Author record, New Author
+  When I click this link
+  Then I am taken to '/authors/new' where I  see a form for a new author record
+  When I fill out the form with a new author's attributes:
+  And I click the button Create Author to submit the form
+  Then a `POST` request is sent to the '/parents' route,
+  a new author record is created,
+  and I am redirected to the Author Index page where I see the new Author displayed" do
+
+    visit "/authors"
+    
+    click_link "New Author"
+
+    fill_in "Author Name", with: "John D"
+    fill_in "Hometown", with: "Denver"
+    check "Currently Writing?"
+    fill_in "Total Awards", with: "7"
+
+    click_button "Create Author"
+
+    expect(page).to have_content("John D")
+    expect(page).to have_content("Denver")
+    expect(page).to have_content("Currently Writing: true")
+    expect(page).to have_content("Total Awards: 7")
+  end
+end
